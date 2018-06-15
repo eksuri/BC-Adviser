552eaf6f5eb5db9d6046b49098efbb72a8b2ac4f
Erik Kristian Sverre Uri <erik.uri@bellevuecollege.edu>
2018-06-15 02:03:29 -0700

    Fix Books Test

--- intents/books/_test.js
+++ intents/books/_test.js
@@ -2,12 +2,10 @@ const conversation = require('alexa-conversation');
 const app = require('../../index.js');
 const config = require('../../config.json');
 
-const opts = {
-    name: 'BookIntent',
-    appId: config.appId,
-    app: app,
-    fixSpaces: true
-};
+let opts = config.opts;
+    opts.name = 'Books Intent';
+    opts.app = app;
+    opts.handler = app.handler;
 
 conversation(opts)
     .userSays('BookIntent', { "CourseAbbrev":"art", "CourseNumber":"101", "ItemNumber":"0650"})
--- intents/books/index.js
+++ intents/books/index.js
@@ -1,62 +1,82 @@
+const Speech = require('ssml-builder');
 const schema = require('./_schema.json')
 exports.schema = schema
 
-const texts = require('../../common/texts');
+const sections = require('../../common/ctc/sections');
+const texts = require('../../common/web/texts');
 
-
-exports.GetBookIntent = function () {
-    console.log("HI")
+exports.GetBookIntent = async function () {
+    let speech = new Speech();
 
     const CourseAbbrev = this.event.request.intent.slots.CourseAbbrev.value;
     const CourseNumber = this.event.request.intent.slots.CourseNumber.value;
     const ItemNumber = this.event.request.intent.slots.ItemNumber.value;
-    let speechOutput;
-    let bookAndAuthor = "";
 
-    if (CourseAbbrev == "" || CourseAbbrev == null)
-    {
-        speechOutput = "You need to provide a course name.";
-    }
-    else if (ItemNumber == "" || ItemNumber == null)
-    {
-        speechOutput = "You need to provide an item number.";
-    } 
-    else 
-    {
-        texts.getTexts(CourseAbbrev, CourseNumber, ItemNumber,  (books) => {
-        
-            if (books != null && books[0] != null)
-            {
-                i = 0;
-                while (books[i] != null) 
-                {
-                    bookAndAuthor = bookAndAuthor + books[i] + ", by " + books[i+1];
-                    if (books[i + 2] != null)
-                    {
-                        bookAndAuthor = bookAndAuthor + ", ";
-                    }
-                    else 
-                    {
-                         bookAndAuthor = bookAndAuthor + ".";
-                    }
-                    i = i + 2;
-                }
-               
-                if (bookAndAuthor.includes("No Textbook Required"))
-                {
-                    speechOutput = "There are no recommended books for " + CourseAbbrev + " " + CourseNumber + " item number " + ItemNumber;
-                }
-                else
-                {
-                    speechOutput = "Books required for " + CourseAbbrev + " " + CourseNumber + " item number " + ItemNumber + " are: " +  bookAndAuthor;
-                }
-            }
-            else {
-                speechOutput = "There are no recommended books for " + CourseAbbrev + " " + CourseNumber + " item number " + ItemNumber;
-            }
-        });
-    }
-    this.response.speak(speechOutput);
-    this.emit(':responseReady');
+    const courses = await sections.getCourseSection(quarter + year, CourseAbbrev, CourseNumber, ItemNumber);
+
+    console.log(courses);
+
 }
 
+/*
+exports.GetBookIntent = async function () {
+    let speech = new Speech();
+
+    const CourseAbbrev = this.event.request.intent.slots.CourseAbbrev.value;
+    const CourseNumber = this.event.request.intent.slots.CourseNumber.value;
+    const ItemNumber = this.event.request.intent.slots.ItemNumber.value;
+
+    let bookAndAuthor = "";
+
+    if (CourseAbbrev == "" || CourseAbbrev == null) {
+        speech.say("You need to provide a course name.")
+    }
+    else if (ItemNumber == "" || ItemNumber == null) {
+        speech.say("You need to provide an item number.")
+    }
+    else {
+        books = await texts.getTexts(CourseAbbrev, CourseNumber, ItemNumber);
+
+        if (books != null && books[0] != null) {
+            i = 0;
+            while (books[i] != null) {
+                bookAndAuthor = bookAndAuthor + books[i] + ", by " + books[i + 1];
+                if (books[i + 2] != null) {
+                    bookAndAuthor = bookAndAuthor + ", ";
+                }
+                else {
+                    bookAndAuthor = bookAndAuthor + ".";
+                }
+                i = i + 2;
+            }
+
+            if (bookAndAuthor.includes("No Textbook Required")) {
+                speech.say("There are no recommended books for")
+                    .say(CourseAbbrev)
+                    .say(CourseNumber)
+                    .say("item number")
+                    .say(ItemNumber)
+            }
+            else {
+                speech.say("Books required for")
+                    .say(CourseAbbrev)
+                    .say(CourseNumber)
+                    .say("item number")
+                    .say(ItemNumber)
+                    .say("are")
+                    .pause("1s")
+                    .say(bookAndAuthor);
+            }
+        }
+        else {
+            speech.say("There are no recommended books for")
+                .say(CourseAbbrev)
+                .say(CourseNumber)
+                .say("item number")
+                .say(ItemNumber);
+        }
+    }
+    this.emit(':tell', speech.ssml(true));
+}
+
+*/
