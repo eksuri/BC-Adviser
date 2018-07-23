d543ff0cdecaeb5901b8a7539487c4a400bc8eb3
mariuspopescu <marius@Mariuss-MacBook-Pro.local>
2018-07-22 22:52:06 -0700

    Quizzes Intent

--- common/canvas/index.js
+++ common/canvas/index.js
@@ -13,7 +13,8 @@ getCanvasCourses = async() => {
 
 getCanvasCourseIds = async () => {
     const classes  = await getCanvasCourses();
-    const ids = classes.map((c) => {return c.ID});
+    //const ids = classes.map((c) => {return c.ID});
+    const ids = classes.map((c) => {return c.id});
 
     return ids;
 }
@@ -36,29 +37,24 @@ getAssignments = async(id) => {
     //or do a more traditional for loop or for each and fill an array
 
     //return something;
-    
-
+    //course_3461B891
+    // "https://canvas.instructure.com/api/v1/announcements?access_token=" + token + "&context_codes[]=course_3477B673"
+    // https://canvas.instructure.com/api/v1/courses/90000001630712/quizzes/?access_token=9~Dk5XtraDbuH48yosqh9J5srFnM3RotJYcb4vHZAJIvRgs15tC6f0maFoYF995VqC&context_codes[]=course_3477B673
+    // const res = await fetch("https://canvas.instructure.com/api/v1/courses/90000001630712/quizzes/?access_token=" + token +"&context_codes[]=course_3477B673");
 }
 
 getQuizzes = async(id) => {
-    return "Test Quiz";
-
-    //you need to 'fetch' the right api url, and pass along your id & token in the request
-    //then you turn your 'res' into 'json' named data
-
-    //const res = await fetch("https://canvas.instructure.com/api/????????????");
-    //const data = await res.json();
-
-    //data will be an array of quizzes
-
-    //then return an array with only the quiz titles
-    //you can use the map function to do it in one line if you want
-    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
-
-    //or do a more traditional for loop or for each and fill an array
-
-    //return something;
-
+    //'fetch' the right api url, and pass along with the id & token in the request
+    const res = await fetch("https://canvas.instructure.com/api/v1/courses/" + id + "/quizzes/?access_token=" + token);
+    //turn 'res' into 'json' named data
+    const data = await res.json();  //data is an array of quizzes
+    if(Array.isArray(data)) {
+        //return an array with only the quiz titles
+        array = data.map((c) => {return c.title}); 
+    } else {
+        array = []
+    }
+    return array;
 }
 
 exports.getAllAssignments = async () => {
@@ -68,12 +64,10 @@ exports.getAllAssignments = async () => {
     return AssignmentNames;
 }
 
-
 exports.getAllQuizzes = async () => {
     const ids = await getCanvasCourseIds();  
-    const QuizNames = ids.map((id) => getQuizzes(id));
-    
-    return QuizNames;
+    const QuizNames = ids.map(async (id) => getQuizzes(id));
+    return Promise.all(QuizNames).then((completed) => { return completed});
 }
 
 exports.getCourseNames = async () => {
