026b0f0c5e67a097ec50ed21d58b91620b422282
Erik Kristian Sverre Uri <erik.uri@bellevuecollege.edu>
2018-08-13 09:34:01 -0700

    Update Schema

--- build_schema.js
+++ build_schema.js
@@ -9,7 +9,6 @@ const subjects = require('./common/ctc/subjects.js');
 
 common_subjects = async () => {
     const data = await subjects.getAllSubjects();
-    console.log(data);
     const name = "SUBJECT";
     const values = data.Subjects.map((s) => {
         let v = {};
